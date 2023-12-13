f9003122b75a7021e640941169de967deae20bd4
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2023-12-13 00:14:47 -0300

    Adicionada a instalação do NodeJS no menu softwares DEV

--- installstart.sh
+++ installstart.sh
@@ -117,6 +117,7 @@ then
     echo "| 3 - MYSQL_SECURE_INSTALATION                                 |"
     echo "| 4 - PHP                                                      |"
     echo "| 5 - Composer                                                 |"
+    echo "| 6 - NodeJS                                                   |"
     echo "| 9 - Retornar ao Menu Principal                               |"
     echo "+--------------------------------------------------------------+"
     read opcaoDev;
@@ -174,11 +175,16 @@ then
         echo -e "\e[01;36mMovendo o Composer para o PATH...\e[00m"
         sudo mv composer.phar /usr/local/bin/composer
         echo -e "\e[01;32mPATH atualizado!\e[00m"
-
-    elif [ $opcao2 == "6" ];
+    elif [ $opcaoDev == "6" ];
     then
-        echo -e "\e[01;36mInstalação do Gimp iniciada...\e[00m"
-        sudo apt install gimp -y
+        echo -e "\e[01;36mInstalação do NodeJS iniciada...\e[00m"
+        sudo apt update
+        sudo apt install -y ca-certificates curl gnupg
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+        NODE_MAJOR=20
+        echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+        sudo apt-get update && sudo apt install nodejs -y
         echo -e "\e[01;32mInstalado com sucesso!\e[00m"
     elif [ $opcao2 == "9" ];
     then
