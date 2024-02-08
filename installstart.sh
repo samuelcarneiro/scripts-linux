1daa0bfd3f577eeebc791b1b3429313bb03c70ef
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-02-08 18:56:02 -0300

    Atualização do instalador do Composer

--- installstart.sh
+++ installstart.sh
@@ -224,7 +224,7 @@ then
     then
         echo -e "\e[01;36mInstalação do Composer iniciada...\e[00m"
         php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-        php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+        php -r "if (hash_file('sha384', 'composer-setup.php') === 'edb40769019ccf227279e3bdd1f5b2e9950eb000c3233ee85148944e555d97be3ea4f40c3c2fe73b22f875385f6a5155') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
         php composer-setup.php
         php -r "unlink('composer-setup.php');"
         echo -e "\e[01;32mInstalado com sucesso!\e[00m"
