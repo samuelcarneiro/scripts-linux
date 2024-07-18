a84f7dd926b37362a97dec21e9468a7957550674
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-07-18 15:47:26 -0300

    Atualização dos comandos, realocação de multimídia e ferramentas de desenvolvedor para a instalação principal. Remoção das opções sem uso do menu

--- start_config.sh
+++ start_config.sh
@@ -22,81 +22,10 @@ install_essential_programs() {
     echo "Instalando programas essenciais..."
     echo ""
     sleep 1
-    sudo apt install qbittorrent gparted neofetch cpufetch gimp inxi gnome-tweaks fonts-firacode ubuntu-restricted-extras mesa-utils git libu2f-udev libfuse2 build-essential curl wget software-properties-common apt-transport-https -y
+    sudo apt install qbittorrent gparted neofetch cpufetch gimp inxi gnome-tweaks fonts-firacode ubuntu-restricted-extras mesa-utils git libu2f-udev libfuse2 build-essential curl wget software-properties-common apt-transport-https vlc ffmpeg python3 python3-pip python3-venv default-jre default-jdk gcc -y
     sudo apt install -f
     echo ""
     echo -e "\e[01;32mInstalações finalizadas!\e[00m"
-    echo ""
-}
-
-# Função para instalação de linguagens de programação
-install_dev_tools() {
-    # Opções de linguagem de programação
-    echo "Selecione qual linguagem de programação irá instalar: "
-    echo "1. Python"
-    echo "2. Java"
-    echo "3. C"
-    echo "0. Voltar..."
-    echo ""
-
-    read -p "Opção: " opcao_lang
-    case $opcao_lang in 
-        1) install_python ;;
-        2) install_java ;;
-        3) install_c ;;
-        0) return ;;
-        *) echo "\e[01;31mOpção inválida! Tente novamente.\e[00m" ;;
-    esac
-    # Função para instalar o Python
-    install_python() {
-        echo ""
-        echo "Instalando Python..."
-        echo ""
-        sleep 1
-        sudo apt install -y python3 python3-pip python3-venv
-        sleep 1
-        echo ""
-        echo -e "\e[01;32mLinguagem de programação Python instalada com sucesso!\e[00m"
-        echo ""
-    }
-
-    # Função para instalar o JAVA
-    install_java() {
-        echo ""
-        echo "Instalando JRE e JDK (JAVA)..."
-        echo ""
-        sleep 1
-        sudo apt install -y default-jre default-jdk
-        sleep 1
-        echo ""
-        echo -e "\e[01;32mLinguagem de programação JAVA instalada com sucesso!\e[00m"
-        echo ""
-    }
-
-    # Função para instalar a linguagem de programação C
-    install_c() {
-        echo ""
-        echo "Instalando C..."
-        echo ""
-        sleep 1
-        sudo apt install -y gcc
-        sleep 1
-        echo ""
-        echo -e "\e[01;32mLinguagem de programação C instalada com sucesso!\e[00m"
-        echo ""
-    }
-}
-
-# Função para instalar pacote multimídia
-install_multimidia() {
-    echo ""
-    echo "Instalando pacotes multimídia..."
-    echo ""
-    sleep 1
-    sudo apt install -y vlc ffmpeg
-    sleep 1
-    echo ""
-    echo -e "\e[01;32mPacotes multimídia instalados com sucesso!\e[00m"
     echo ""
 }
 
@@ -133,10 +62,8 @@ while true; do
     echo "--------------------------------------------"
     echo "1. Atualizar sistema operacional            "
     echo "2. Instalar programas essenciais            "
-    echo "3. Instalar ferramentas de desenvolvedor    "
-    echo "4. Instalar pacotes multimídia              "
-    echo "5. Realizar limpeza lógica                  "
-    echo "6. Atalhos de comandos no .bashrc           "
+    echo "3. Realizar limpeza lógica                  "
+    echo "4. Atalhos de comandos no .bashrc           "
     echo "0. Sair                                     "
     echo "--------------------------------------------"
 
@@ -145,10 +72,8 @@ while true; do
     case $opcao in
         1) update_system ;;
         2) install_essential_programs ;;
-        3) install_dev_tools ;;
-        4) install_multimidia ;;
-        5) logic_limp ;;
-        6) shortcut ;;
+        3) logic_limp ;;
+        4) shortcut ;;
         0) echo "Saindo..." ; break ;;
         *) echo -e "\e[01;31mOpção inválida! Tente novamente.\e[00m" ;;
     esac
