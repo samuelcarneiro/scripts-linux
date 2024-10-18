8f57a6546d14bf0e85975be69ce2e2726035f9cf
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-10-17 22:34:31 -0300

    Adicionada função para a instalação e configuração de um servidor web local com PHP e MySQL

--- start_config_version_deb.sh
+++ start_config_version_deb.sh
@@ -46,6 +46,60 @@ install_dev_programs() {
     echo ""
 }
 
+# Função para instalação e configuração de um servidor WEB local
+# com PHP e MySQL
+install_web_server() {
+    echo -e "\e[01;36mInstalação do Servidor WEB iniciada...\e[00m"
+    echo "---------------------------------"
+    echo -e "\e[01;36mInstalação do APACHE2 iniciada...\e[00m"
+    sleep 1
+    sudo apt install apache2 libapache2-mod-php -y
+    echo -e "\e[01;32mInstalado com sucesso!\e[00m"
+    sleep 1
+    echo -e "\e[01;36mHabilitando Mod_Rewrite...\e[00m"
+    sudo a2enmod rewrite
+    echo -e "\e[01;32mMod_Rewrite habilidado!\e[00m"
+    sleep 1
+    echo -e "\e[01;36mReiniciando servidor APACHE2...\e[00m"
+    sudo systemctl restart apache2
+    echo -e "\e[01;32m[ APACHE2 OK ]\e[00m"
+    echo "---------------------------------"
+    sleep 1
+    echo -e "\e[01;36mInstalação do MySQL iniciada...\e[00m"
+    sudo apt install mysql-server -y
+    echo -e "\e[01;32mInstalado com sucesso!\e[00m"
+    echo ""
+    sleep 1
+    echo -e "\e[01;36mIniciando configuração do MySQL...\e[00m"
+    echo -e "\e[01;33mConfigurando a nova senha do MySQL\e[00m"
+    echo -e "\e[01;33mInforme uma nova senha para o banco de dados:\e[00m"
+    read passwdMysql;
+    sudo mysql --user=root --password='' -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$passwdMysql';"
+    echo -e "\e[01;33mSenha alterada com sucesso!\e[00m"
+    echo -e "\e[01;32m[ MYSQL OK ]\e[00m"
+    echo "---------------------------------"
+    sleep 1
+    echo -e "\e[01;36mConfiguração MYSQL_SECURE_INSTALATION iniciada...\e[00m"
+    sudo mysql_secure_installation
+    echo -e "\e[01;32mConfiguração finalizada!\e[00m"
+    echo ""
+    echo -e "\e[01;36mReiniciando servidor MYSQL...\e[00m"
+    sudo systemctl restart mysql.service
+    echo -e "\e[01;32m[ SECURE INSTALATION MYSQL OK ]\e[00m"
+    echo "---------------------------------"
+    sleep 1
+    echo -e "\e[01;36mInstalação do PHP iniciada...\e[00m"
+    sudo apt install php php-xml php-curl php-mbstring php-mysql -y
+    echo -e "\e[01;32mInstalação finalizada!\e[00m"
+    echo ""
+    sleep 1
+    echo -e "\e[01;36mReiniciando servidor...\e[00m"
+    sudo systemctl restart apache2
+    sudo systemctl restart mysql.service
+    echo -e "\e[01;32m[ SERVIDOR OK ]\e[00m"
+    echo -e "\e[01;32m[ Servidor WEB instalado e configurado com sucesso!! ]\e[00m"
+}
+
 # Função para realizar limpeza lógica...
 logic_limp() {
     echo ""
@@ -80,8 +134,9 @@ while true; do
     echo "1. Atualizar sistema operacional            "
     echo "2. Instalar programas essenciais            "
     echo "3. Instalar ferramentas DEV                 "
-    echo "4. Realizar limpeza lógica                  "
-    echo "5. Atalhos de comandos no .bashrc           "
+    echo "4. Instalar Web Server Local (PHP&MySQL)    "
+    echo "5. Realizar limpeza lógica                  "
+    echo "6. Atalhos de comandos no .bashrc           "
     echo "0. Sair                                     "
     echo "--------------------------------------------"
 
@@ -91,8 +146,9 @@ while true; do
         1) update_system ;;
         2) install_essential_programs ;;
         3) install_dev_programs ;;
-        4) logic_limp ;;
-        5) shortcut ;;
+        4) install_web_server ;;
+        5) logic_limp ;;
+        6) shortcut ;;
         0) echo "Saindo..." ; break ;;
         *) echo -e "\e[01;31mOpção inválida! Tente novamente.\e[00m" ;;
     esac
